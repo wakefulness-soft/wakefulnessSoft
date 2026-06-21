39331f42610d915633acc5a1ee3dea2619ed1b89
Braulio Garcia <brauliogrc95@gmail.com>
2026-06-20 19:42:13 -0600

    Change project to TS and TSX

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import './App.css'
+import Navbar from './components/Navbar.tsx'
+import Footer from './components/Footer.tsx'
+
+function App() {
+  return (
+    <>
+      <div className="page-container">
+        <Navbar />
+        <div className="main-content pt-20">
+          <h1>Wakefulnes Software</h1>
+        </div>
+        <Footer />
+      </div>
+    </>
+  )
+}
+
+export default App
