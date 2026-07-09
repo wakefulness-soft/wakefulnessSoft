ff6ebfcc42237a92cf601d84bdf5c9b67b0e852f
Braulio Garcia <brauliogrc95@gmail.com>
2026-07-09 16:58:49 -0600

    Hero section initial design

--- src/components/HeroSection/HeroSection.tsx
+++ src/components/HeroSection/HeroSection.tsx
@@ -8,13 +8,34 @@ export const HeroSection = () => {
                     banner de AI
                 </div> */}
                 
-                <h1 className="mt-6 text-balance text-4xl font-semibold leading-[1.05] tracking-tight">
+                <h1 className="mt-6 text-balance text-4xl font-semibold leading-[1.05] tracking-tight sm:text-6xl lg:text-7xl">
                     We turn bold ideas into
                     <span className="block text-mypurple">
                         software that wakes up
                     </span>
                     <span className="block">your business.</span>
                 </h1>
+
+                <p className="mx-auto mt-6 max-w-xl text-pretty text-base leading-relaxed sm:text-lg">
+                    Designing your dream is our most cherished task
+                </p>
+
+                <div className="mt-9 flex flex-col items-center justify-center gap-6 md:flex-row">
+                    {/* TODO: Add href */}
+                    <a
+                        href=""
+                        className="bg-mypurple text-darkvoid px-4 py-2 rounded-xl"
+                    >
+                        Request a Quote
+                    </a>
+                    {/* TODO: Add href */}
+                    <a
+                        href=""
+                        className="border-solid border px-4 py-2 rounded-xl"
+                    >
+                        View Our Work
+                    </a>
+                </div>
             </div>
         </div>
     </section>
