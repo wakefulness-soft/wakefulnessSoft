e32cb078852ea4e585421e80bf4ef6a669b95955
JosueReyes <petbook51@hotmail.com>
2026-07-21 11:41:50 -0600

    About section

---
+++ src/pages/Home/HomePage.tsx
@@ -0,0 +1,25 @@
+import { CtaSection } from "../../components/CtaSection/CtaSection"
+import { HeroSection } from "../../components/HeroSection/HeroSection"
+import { ProcessSection } from "../../components/ProcessSection/ProcessSection"
+import { ProjectsSection } from "../../components/ProjectsSection/ProjectsSection"
+import { ServicesSection } from "../../components/ServicesSection/ServicesSection"
+import { TestimonialSection } from "../../components/TestimonialSection/TestimonialSection"
+import { WhySection } from "../../components/WhySection/WhySection"
+import AboutSection from "../About/About"
+
+export const HomePage = () => {
+  return (
+    <div className="relative min-h-screen">
+      <main>
+        <HeroSection />
+        <ServicesSection />
+        <AboutSection />
+        <WhySection />
+        <ProcessSection />
+        <ProjectsSection />
+        <TestimonialSection />
+        <CtaSection />
+      </main>
+    </div>
+  )
+}
