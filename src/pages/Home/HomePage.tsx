83c1df875edd5acfc8562b2f9150ab239508bf1d
JosueReyes <petbook51@hotmail.com>
2026-08-01 15:18:44 -0600

    ProjectsSection

--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -1,7 +1,7 @@
 import { CtaSection } from "../../components/CtaSection/CtaSection"
 import { HeroSection } from "../../components/HeroSection/HeroSection"
 import { ProcessSection } from "../../components/ProcessSection/ProcessSection"
-import { ProjectsSection } from "../../components/ProjectsSection/ProjectsSection"
+import  ProjectsSection  from "../../components/ProjectsSection/ProjectsSection"
 import { ServicesSection } from "../../components/ServicesSection/ServicesSection"
 import { TestimonialSection } from "../../components/TestimonialSection/TestimonialSection"
 import { WhySection } from "../../components/WhySection/WhySection"
