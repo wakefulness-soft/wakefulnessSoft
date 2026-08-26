9385dad9e9fcd995032f8531153054e7a32889e3
Braulio Garcia <brauliogrc95@gmail.com>
2026-08-26 13:17:48 -0600

    Corrections in language switch
    - Move language switch to components/ui directory
    - Update imports
    - Change language switch behavior and style

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import Navbar from './components/Navbar/Navbar.tsx'
 import Footer from './components/Footer/Footer.tsx'
-import LanguageSwitcher from "./components/LanguageSwitcher/LanguageSwitcher";
+import LanguageSwitcher from "./components/ui/LanguageSwitcher/LanguageSwitcher";
 import { HomePage } from './pages/Home/HomePage'
 import { ThemeSwitcher } from './components/ui/ThemeSwitcher/ThemeSwitcher.tsx'
 import { ThemeContextProvider } from './context/ThemeContext.tsx'
